6c751580564487e8bb243f58f900a5bce10db123
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-13 19:10:41 +0000

    Update LiberarySystem.java

--- cs4004_13-11-2022/v2/src/main/java/org/example/LiberarySystem.java
+++ cs4004_13-11-2022/v2/src/main/java/org/example/LiberarySystem.java
@@ -106,7 +106,46 @@ public class LiberarySystem{
         int[] daysOfMonth = {13,28,31,30,31,30,31,31,30,31,30,31};
         return Integer.parseInt(dateChop[0]) + daysOfMonth[Integer.parseInt(dateChop[1]) - 1] + (365 * Integer.parseInt(dateChop[2]));
     }
+ public static void main(String[] args) 
+    {
+    Person p = new Person();
 
+  public static string emailCheck(String email) {
+    if(!email.contains("@") || !email.contains(".") || !email.matches(".*[a-zA-Z].*")){//makes sure email is an email
+        throw new emailException("Cannot add this email. That's not an email");
+    return emailCheck;
+
+  }
+  
+
+  //scanner starts
+  public static void main(String[] args) { 
+    Scanner input = new Scanner(System.in);
+    
+    //enter name, email, department(s)
+    System.out.print("Enter Name and Email: ");
+    String name = input.next();
+    String email = input.next();
+    String departments = input.next(); 
+
+    
+    try {
+      this.name = p.name;
+      this.email = p.email;
+      String[] arr = p.departments.split(", ");//splits up departments string and adds to arraylist
+      this.departments.addAll(Arrays.asList(arr));
+
+      
+      
+    }
+    catch (emailException ex) {
+      System.out.println("Please try again.");
+    }
+
+    System.out.println("Execution continues ...");
+  }
+}
+}
 
 
 }
