232509f262bf9ad8c60fefb053136c83478d0f71
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-14 16:15:42 +0000

    Add files via upload

--- cs4004_13-11-2022/v2/src/main/java/org/example/Reservation.java
+++ cs4004_13-11-2022/v2/src/main/java/org/example/Reservation.java
@@ -1,4 +1,4 @@
- 
+package org.example;
 
 import java.time.LocalDate;
 
@@ -6,6 +6,7 @@ public class Reservation extends Loan{
    
     private int reservationDateOffset;
     private LocalDate reservationDate;
+    
 
     Reservation(Book book, Person loanedTo, LocalDate reservationDate)throws RuntimeException{
 
@@ -17,7 +18,18 @@ public class Reservation extends Loan{
         super.setDueDate(reservationDate.plusWeeks(3));//3 week loan)
 
         
-        
     }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+
+    } 
+
+    public void setReservationDate(LocalDate reservationDate){
+        this.reservationDate = reservationDate;
+
+    }   
+
+}
 
   
