a32585460c8df414313b5e394e58ce0d1061fa81
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-14 18:34:14 +0000

    Update Reservation.java

--- cs4004_13-11-2022/v2/src/main/java/org/example/Reservation.java
+++ cs4004_13-11-2022/v2/src/main/java/org/example/Reservation.java
@@ -4,20 +4,14 @@ import java.time.LocalDate;
 
 public class Reservation extends Loan{
    
-    private int reservationDateOffset;
+   
     private LocalDate reservationDate;
-    
 
-    Reservation(Book book, Person loanedTo, LocalDate reservationDate)throws RuntimeException{
-
-
+    Reservation(Book book, Person loanedTo, String reservationString)throws RuntimeException{
         super(book, loanedTo);
-        reservationDate = LocalDate.now();
-        reservationDate =  reservationDate.plusDays(reservationDateOffset);
+        reservationDate = LocalDate.parse(reservationString); // converts string to LocalDate
         super.setDateTaken(reservationDate);
         super.setDueDate(reservationDate.plusWeeks(3));//3 week loan)
-
-        
     }
 
     public LocalDate getReservationDate() {
